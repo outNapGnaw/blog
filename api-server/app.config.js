594b5cbbc1ab3f2643c38bdfc6b82db8b20f3a66
peyton <WPTWWJ@163.com>
2018-12-13 23:27:55 +0800

    update refree

--- api-server/app.config.js
+++ api-server/app.config.js
@@ -20,7 +20,7 @@ exports.APP = {
 
 exports.CROSS_DOMAIN = {
 	allowedOrigins: ['http://peytonis.aitboy.cn', 'http://cdn.aitboy.cn', 'http://peytonblogadmin.aitboy.cn:8088'],
-	allowedReferer: 'peytonis.aitboy.cn'
+	allowedReferer: 'aitboy.cn'
 }
 
 exports.MONGODB = {
