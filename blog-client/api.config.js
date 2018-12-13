eda91df823131d6eca70c830b1860e2876b30f43
peyton <wangpantuo@quancheng-ec.com>
2018-12-13 19:07:29 +0800

    update http

--- blog-client/api.config.js
+++ blog-client/api.config.js
@@ -5,11 +5,11 @@
  */
 
 const { isProdMode } = require('./environment')
-
+const httpORhttps = process.env.protocol || 'http'
 module.exports = {
   // cdnUrl: isProdMode ? 'https://peytonblog.aitboy.cn' : '',
   cdnUrl: isProdMode ? '' : '',
-  proxyUrl: isProdMode ? 'https://peytonis.aitboy.cn/proxy/' : '/proxy/',
-  baseUrl: isProdMode ? 'https://peytonblogapi.aitboy.cn' : 'http://localhost:8000',
-  socketHost: isProdMode ? 'https://peytonis.aitboy.cn' : 'http://localhost:3000'
+  proxyUrl: isProdMode ? `${httpORhttps}://peytonis.aitboy.cn/proxy/` : '/proxy/',
+  baseUrl: isProdMode ? `${httpORhttps}://peytonblogapi.aitboy.cn` : 'http://localhost:8000',
+  socketHost: isProdMode ? `${httpORhttps}://peytonis.aitboy.cn` : 'http://localhost:3000'
 }
